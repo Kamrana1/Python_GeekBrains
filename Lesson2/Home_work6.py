9d03c3ad783e1b28c044c2bdd4df7cdd28fd5ec5
Kamran <k.aliyev@w-t.az>
2023-12-06 13:44:20 +0400

    Tapşırıq 6
    * Реализовать структуру данных «Товары». Она должна представлять собой список кортежей. Каждый кортеж хранит информацию об отдельном товаре. В кортеже должно быть два элемента — номер товара и словарь с параметрами (характеристиками товара: название, цена, количество, единица измерения). Структуру нужно сформировать программно, т.е. запрашивать все данные у пользователя.
    Пример готовой структуры:
    [
    (1, {“название”: “компьютер”, “цена”: 20000, “количество”: 5, “eд”: “шт.”}),
    (2, {“название”: “принтер”, “цена”: 6000, “количество”: 2, “eд”: “шт.”}),
    (3, {“название”: “сканер”, “цена”: 2000, “количество”: 7, “eд”: “шт.”})
    ]
    Необходимо собрать аналитику о товарах. Реализовать словарь, в котором каждый ключ — характеристика товара, например название, а значение — список значений-характеристик, например список названий товаров.
    Пример:
    {
    “название”: [“компьютер”, “принтер”, “сканер”],
    “цена”: [20000, 6000, 2000],
    “количество”: [5, 2, 7],
    “ед”: [“шт.”]
    }

--- Lesson2/Home_work6.py
+++ Lesson2/Home_work6.py
@@ -1,22 +1,40 @@
-i=1
-my_list=[]
+i = 1
+my_list = []
+
 while True:
-    Productname=input("Введите название товара: ")
-    Productprice=input("Введите цену товара: ")
-    Productcount=input("Введите количество товара: ")
-    Productunit=input("Введите единицу измерение: ")
-    a = (i, {"Название: ":Productname,"Цена: ": Productprice,"Количество: ": Productcount,"Единица измерение: ": Productunit})
+    Productname = input("Введите название товара: ")
+    Productprice = float(input("Введите цену товара: "))  # Преобразуем в число
+    Productcount = int(input("Введите количество товара: "))  # Преобразуем в число
+    Productunit = input("Введите единицу измерения: ")
+
+    a = (i, {"name": Productname, "price": Productprice, "count": Productcount, "unit": Productunit})
     my_list.append(a)
+
     print("Продолжить: 1")
     print("Выход: 2")
-    i=int(input("Введите выбор: "))
-    if i==2:
+    i = int(input("Введите выбор: "))
+
+    if i == 2:
         break
 
 for elem in my_list:
     print(f"{elem}")
 
-prodnamelist=[]
+prodnamelist = []
+prodpricelist=[]
+prodcountlist=[]
+productunitlist=[]
+
 for elem in my_list:
-    for elem1 in elem[1]:
-        prodnamelist.append(elem1[values])
+    prodnamelist.append(elem[1]["name"])
+    prodpricelist.append(elem[1]["price"])
+    prodcountlist.append(elem[1]["count"])
+    productunitlist.append(elem[1]["unit"])
+
+my_dict = {}
+my_dict["name"] = prodnamelist
+my_dict["price"] = prodpricelist
+my_dict["count"] = prodcountlist
+my_dict["unit"] = productunitlist
+
+print(my_dict)
