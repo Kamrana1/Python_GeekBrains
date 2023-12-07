ebc7aad2d5d7042d242fc6a5d382a4052e58f32b
Kamran <k.aliyev@w-t.az>
2023-12-07 17:02:31 +0400

    Tapşırıq2
    Реализовать функцию, принимающую несколько параметров, описывающих данные пользователя: имя, фамилия, год рождения, город проживания, email, телефон. Функция должна принимать параметры как именованные аргументы. Реализовать вывод данных о пользователе одной строкой.

---
+++ Lesson3/Home_work2.py
@@ -0,0 +1,5 @@
+def user(name, surname, age, city, email, phone):
+    print(f"Имя: {name}, Фамилия: {surname}, Город проживание: {city}, эмаил: {email}, телефон: {phone}, Возраст: {age}")
+
+user(name="Kamran", surname="Aliyev", email="kaaliyev@inbox.ru", city="Baku", phone="+994502070305", age=26)
+
