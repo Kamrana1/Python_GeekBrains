5732a2b3f783c7710481ef81ebc86b33e50dcd06
Kamran <k.aliyev@w-t.az>
2023-12-25 10:53:59 +0400

    Tapşırıq5
    Создать (программно) текстовый файл, записать в него программно набор чисел, разделенных пробелами. Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
    6. Необходимо создать (не программно) текстовый файл, где каждая строка описывает учебный предмет и наличие лекционных, практических и лабораторных занятий по этому предмету и их количество. Важно, чтобы для каждого предмета не обязательно были все типы занятий. Сформировать словарь, содержащий название предмета и общее количество занятий по нему. Вывести словарь на экран.
    Примеры строк файла:
    Информатика: 100(л) 50(пр) 20(лаб).
    Физика: 30(л) — 10(лаб)
    Физкультура: — 30(пр) —
    
    Пример словаря:
    {“Информатика”: 170, “Физика”: 40, “Физкультура”: 30}

---
+++ Lesson5/Home_work6/Home_work6.py
@@ -0,0 +1,29 @@
+file_path = 'subjects.txt'
+
+subject_dict = {}  # Инициализация словаря для хранения данных
+
+try:
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            parts = line.strip().split(':')
+            if len(parts) == 2:
+                subject_name = parts[0].strip()
+                lesson_types = parts[1].split()
+                total_lessons = 0
+
+                for lesson in lesson_types:
+                    if '(' in lesson and ')' in lesson:
+                        lesson_count = int(lesson[lesson.find('(')+1:lesson.find(')')])
+                        total_lessons += lesson_count
+
+                subject_dict[subject_name] = total_lessons
+            elif line.strip():  # Проверка, что строка не пуста
+                print(f"Пропущена строка с некорректным форматом: {line}")
+
+    print("Словарь с общим количеством занятий:")
+    print(subject_dict)
+
+except FileNotFoundError:
+    print(f"Файл '{file_path}' не найден.")
+except Exception as e:
+    print(f"Произошла ошибка: {e}")
